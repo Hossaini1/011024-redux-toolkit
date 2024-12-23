a8d435e4424268722e81ade107996a0fcd363551
ali <1989hossaini1@gmail.com>
2024-12-23 20:27:16 +0100

    culculate logik erstellt plus die modal für entfernung alle items die sich in warenkorb befinden

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Navbar from "./component/Navbar";
 import CartContainer from "./component/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./component/Modal";
+
 
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(calculateTotals())
+  },[cartItems])
+
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
